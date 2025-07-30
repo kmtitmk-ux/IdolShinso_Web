7016358498a935dd63b42aa54b1e23ab663a02c1
Kazuki Matsui <kmtitmk123@gmail.com>
2025-07-30 15:30:16 +0900

    戻す

--- package/amplify/functions/my-first-function/handler.ts
+++ package/amplify/functions/my-first-function/handler.ts
@@ -1,15 +1,4 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-
 export const handler = async (event: any) => {
-    const url = 'https://c-ute.doorblog.jp/';
-    const res = await axios.get(url);
-    const $ = cheerio.load(res.data);
-    const title = $('title').text();
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ title }),
-    };
-
+    console.log("Event received:", event);
+    return "Hello from my first function!";
 };
