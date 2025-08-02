11dd05d360d313706386fbd6dde12d8f73b4b1cb
Kazuki Matsui <kmtitmk123@gmail.com>
2025-08-02 23:36:54 +0900

    関数追加テスト

--- package/amplify/backend.ts
+++ package/amplify/backend.ts
@@ -2,12 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { myFirstFunction } from './functions/my-first-function/resource';
-
+import { sayHello } from './functions/say-hello/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
+  sayHello,
   auth,
   data,
   myFirstFunction
