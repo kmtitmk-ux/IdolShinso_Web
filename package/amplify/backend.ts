fdd34ab33bff41b046c6d7c34d48e3b929d14c85
Kazuki Matsui <kmtitmk123@gmail.com>
2025-08-16 06:48:12 +0900

    ストレージ追加

--- package/amplify/backend.ts
+++ package/amplify/backend.ts
@@ -2,12 +2,13 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { myFirstFunction } from './functions/my-first-function/resource';
-
+import { storage } from './storage/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
   data,
-  myFirstFunction
+  myFirstFunction,
+  storage
 });
