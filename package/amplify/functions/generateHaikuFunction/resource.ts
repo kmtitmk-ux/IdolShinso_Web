663c52b2d56d7d3e3ff743c8ebde373b9ff0cf6f
Kazuki Matsui <kmtitmk123@gmail.com>
2025-08-19 06:43:02 +0900

    環境変数変更

--- package/amplify/functions/generateHaikuFunction/resource.ts
+++ package/amplify/functions/generateHaikuFunction/resource.ts
@@ -2,22 +2,13 @@ import { defineFunction } from "@aws-amplify/backend";
 
 const envConfig = {
     main: {
-        TABLE_NAME_IS01: "IS01-2w6gn26ifzccdn6xdivunqamhu-NONE",
-        TABLE_NAME_IS02: "IS02-2w6gn26ifzccdn6xdivunqamhu-NONE",
-        TABLE_NAME_IS03: "IS03-2w6gn26ifzccdn6xdivunqamhu-NONE",
-        BUCKET_NAME_IS01: "amplify-dtb1zhx1jvcon-main-bran-is01bucketd5d9d3bb-pxrydzwv0aeo"
+        MODEL_ID: "anthropic.claude-3-haiku-20240307-v1:0",
     },
     develop: {
-        TABLE_NAME_IS01: "IS01-o4ipk765lnd2fm3ecrgqjnhbsq-NONE",
-        TABLE_NAME_IS02: "IS02-o4ipk765lnd2fm3ecrgqjnhbsq-NONE",
-        TABLE_NAME_IS03: "IS03-o4ipk765lnd2fm3ecrgqjnhbsq-NONE",
-        BUCKET_NAME_IS01: "amplify-dtb1zhx1jvcon-develop-b-is01bucketd5d9d3bb-xytrkowiqmzx"
+        MODEL_ID: "anthropic.claude-3-haiku-20240307-v1:0",
     },
     local: {
-        TABLE_NAME_IS01: "IS01-lwae74brlvfstbdkoxvvvdcofq-NONE",
-        TABLE_NAME_IS02: "IS02-lwae74brlvfstbdkoxvvvdcofq-NONE",
-        TABLE_NAME_IS03: "IS03-lwae74brlvfstbdkoxvvvdcofq-NONE",
-        BUCKET_NAME_IS01: "amplify-modernizenextfree-kmtit-is01bucketd5d9d3bb-8lyst8isl15o"
+        MODEL_ID: "anthropic.claude-3-haiku-20240307-v1:0"
     }
 };
 type Branch = "main" | "develop";
@@ -29,11 +20,8 @@ export const generateHaikuFunction = defineFunction({
     entry: "./handler.ts",
     timeoutSeconds: 900,
     environment: {
-        TABLE_NAME_IS01: selectedConfig.TABLE_NAME_IS01,
-        TABLE_NAME_IS02: selectedConfig.TABLE_NAME_IS02,
-        TABLE_NAME_IS03: selectedConfig.TABLE_NAME_IS03,
-        BUCKET_NAME_IS01: selectedConfig.BUCKET_NAME_IS01
+        MODEL_ID: selectedConfig.MODEL_ID
     }
 });
 
-export const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
+export const MODEL_ID = envConfig[branch].MODEL_ID;
