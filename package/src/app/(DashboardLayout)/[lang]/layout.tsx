1685931be10f4c747970642ad3e5596efa51e13e
Kazuki Matsui <kmtitmk123@gmail.com>
2026-06-11 16:46:57 +0900

    スクリプトタグ修正

--- package/src/app/(DashboardLayout)/[lang]/layout.tsx
+++ package/src/app/(DashboardLayout)/[lang]/layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Box } from "@mui/material";
-import Script from "next/script";
 import ClientLayout from "@/src/app/(DashboardLayout)/ClientLayout";
 import ClientThemeProvider from "@/app/ClientThemeProvider";
 
@@ -18,14 +17,14 @@ export default async function RootLayout({
             <html lang={lang} suppressHydrationWarning>
                 <head>
                     {/* Google Tag Manager */}
-                    <Script id="gtm-script" strategy="afterInteractive">
+                    <script id="gtm-script" strategy="afterInteractive">
                         {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','GTM-KG6DKBNT');
                         `}
-                    </Script>
+                    </script>
                 </head>
                 <body>
                     {/* Google Tag Manager (noscript) */}
@@ -35,7 +34,7 @@ export default async function RootLayout({
                         }
                     } />
                     {/* End Google Tag Manager (noscript) */}
-                    <Script
+                    <script
                         id="adsense-script"
                         async
                         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9862851994514309"
