de635242d1fae4aebc9501f14cc4103531b0dde8
Kazuki Matsui <kmtitmk123@gmail.com>
2026-06-12 10:39:17 +0900

    スクリプトタグエラー修正

--- package/src/app/(DashboardLayout)/[lang]/layout.tsx
+++ package/src/app/(DashboardLayout)/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Box } from "@mui/material";
 import ClientLayout from "@/src/app/(DashboardLayout)/ClientLayout";
 import ClientThemeProvider from "@/app/ClientThemeProvider";
+import Script from 'next/script';
 
 export default async function RootLayout({
     children,
@@ -17,25 +18,25 @@ export default async function RootLayout({
             <html lang={lang} suppressHydrationWarning>
                 <head>
                     {/* Google Tag Manager */}
-                    <script id="gtm-script" strategy="afterInteractive">
+                    <Script id="gtm-script" strategy="afterInteractive">
                         {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-KG6DKBNT');
-                        `}
-                    </script>
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KG6DKBNT');`}
+                    </Script>
                 </head>
                 <body>
                     {/* Google Tag Manager (noscript) */}
-                    <noscript dangerouslySetInnerHTML={
-                        {
-                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG6DKBNT" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-                        }
-                    } />
-                    {/* End Google Tag Manager (noscript) */}
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9862851994514309"
-                        crossorigin="anonymous"></script>
+                    <Script id="gtm-noscript" strategy="afterInteractive">
+                        {`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG6DKBNT" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}
+                    </Script>
+                    {/* Google AdSense */}
+                    <Script
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9862851994514309"
+                        crossOrigin="anonymous"
+                        strategy="afterInteractive" 
+                    />
                     <ClientThemeProvider>
                         <ClientLayout>
                             {/* PageContent */}
